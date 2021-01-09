31b228da53e01e47b26959e6dcd39b6f67438816
ogunsoladebayo <ogunsoladebayo@gmail.com>
2021-01-09 23:42:15 +0100

    feat: added login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 const Wallet = require('../models/Wallet');
 
 // @desc      Register user
-// @route     POST /api/v1/auth/register
+// @route     POST /v1/auth/register
 // @access    Public
 exports.register = asyncHandler(async (req, res, next) => {
 	const { name, email, password, currency } = req.body;
@@ -44,6 +44,36 @@ exports.register = asyncHandler(async (req, res, next) => {
 
 	sendTokenResponse(user, 200, res);
 	user.save({ validateBeforeSave: false });
+});
+
+// @desc      Login user
+// @route     POST /v1/auth/login
+// @access    Public
+exports.login = asyncHandler(async (req, res, next) => {
+	const { email, password } = req.body;
+
+	// Validate emil & password
+	if (!email || !password) {
+		return next(
+			new ErrorResponse('Please provide an email and password', 400)
+		);
+	}
+
+	// Check for user
+	const user = await User.findOne({ email }).select('+password');
+
+	if (!user) {
+		return next(new ErrorResponse('Invalid credentials', 401));
+	}
+
+	// Check if password matches
+	const isMatch = await user.matchPassword(password);
+
+	if (!isMatch) {
+		return next(new ErrorResponse('Invalid credentials', 401));
+	}
+
+	sendTokenResponse(user, 200, res);
 });
 
 // Get token from model, create cookie and send response
--- models/User.js
+++ models/User.js
@@ -87,4 +87,9 @@ UserSchema.methods.generateEmailConfirmToken = function (next) {
 	return confirmTokenCombined;
 };
 
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model('User', UserSchema);
