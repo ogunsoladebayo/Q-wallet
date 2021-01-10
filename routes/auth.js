b855ddaeee1eefa795c5911bdd5ce010d798006e
ogunsoladebayo <ogunsoladebayo@gmail.com>
2021-01-10 03:47:03 +0100

    feat: added logout functionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { register, login } = require('../controllers/auth');
+const { register, login, logout } = require('../controllers/auth');
 
 const router = express.Router();
 
-router.post('/register', register).post('/login', login);
+router.post('/register', register).post('/login', login).get('logout', logout);
 
 module.exports = router;
