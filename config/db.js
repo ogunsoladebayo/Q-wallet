e88880883a20b1c7f601289c966ca86a645ba547
ogunsoladebayo <ogunsoladebayo@gmail.com>
2021-01-16 10:20:06 +0100

    changed mongo url for deployment

--- config/db.js
+++ config/db.js
@@ -10,7 +10,9 @@ const {
 	MONGO_PORT,
 	MONGO_DB,
 } = process.env;
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url =
+	'mongodb+srv://q-wallet:uEvOiDVoE1Qe3M3P@devhub.wzqeb.mongodb.net/q-wallet?retryWrites=true&w=majority';
 
 const connectDB = async () => {
 	const conn = await mongoose.connect(url, {
